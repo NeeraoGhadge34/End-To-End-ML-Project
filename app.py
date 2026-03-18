a620e51ad8d9d5a1e6fb7bf7f7a1de666a7bf99b
Neerao Ghadge <neeraoghadge386@gmail.com>
2026-03-19 03:26:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from flask import Flask, request, render_template
 
+
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
